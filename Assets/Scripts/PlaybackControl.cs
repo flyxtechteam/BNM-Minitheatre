ddd372c9641eb81fe7e6d37c3cb3a88908aae212
Flyx Technologies <flyxtechteam@gmail.com>
2017-07-27 18:37:54 +0800

    Added Scene 6, added some scripts, added playback control
    - Added PlaybackControl script to play/pause demo if viewer start/stops cycling
    - Imported new batch of assets
    - Added UI fade animation for pause overlay
    - Updated Scene 6 (Eco City) - placed props and setup lighting
    - Added editor script to export Terrain object to OBJ format

---
+++ Assets/Scripts/PlaybackControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaybackControl : MonoBehaviour
+{
+	[SerializeField]
+	KeyCode cycleKey = KeyCode.W;
+
+    [SerializeField]
+    Animator pauseOverlay;
+
+    [SerializeField]
+    float pauseDelay = 5f;
+
+    bool pause = false;
+    float scaleRate = 0.05f;
+    float targetTimeScale = 0f;
+    float LerpThreshold = 0.05f;
+
+
+    void Update ()
+	{
+		if ((!Input.GetKey(cycleKey)) && (!pause))
+		{
+            StartCoroutine("DelayPause");
+            pause = true;
+        }
+		
+		if (Input.GetKeyDown(cycleKey))
+		{ 
+            StopCoroutine("DelayPause");
+            targetTimeScale = 1f;
+            pause = false;
+
+            pauseOverlay.SetBool("paused", false);
+        }
+
+        if (targetTimeScale == 1f)
+        {
+            if (Time.timeScale < (1f - LerpThreshold))
+            {
+                Time.timeScale = Mathf.Lerp(Time.timeScale, targetTimeScale, scaleRate);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+        else
+        {
+            if (targetTimeScale == 0f)
+            {
+                if (Time.timeScale > LerpThreshold)
+                {
+                    Time.timeScale = Mathf.Lerp(Time.timeScale, targetTimeScale, scaleRate);
+                }
+                else
+                {
+                    Time.timeScale = 0f;
+                }
+            }
+        }
+	}
+
+    IEnumerator DelayPause()
+    {
+        yield return new WaitForSeconds(pauseDelay);
+
+        if (pause == true)
+        {
+            targetTimeScale = 0f;
+            pauseOverlay.SetBool("paused", true);
+        }
+    }
+}
