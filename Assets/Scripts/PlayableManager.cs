f3b71806ab05d6960aa513acd12c84157b51a4e4
Flyx Technologies <flyxtechteam@gmail.com>
2017-09-19 18:46:11 +0800

    Implemented plugin, changes to structure to accomodate plugin
    - Changed to persistent camera that persists throughout all scenes
    - Changed Canvas to world space, camera setup
    - Updated Scene 1, 2, and 3 to work with new structure

---
+++ Assets/Scripts/PlayableManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class PlayableManager : MonoBehaviour
+{
+    [SerializeField]
+    PlayableAsset[] playables;
+
+    int sceneCounter = 0;
+
+    PlayableDirector timeline;
+    PlaybackControl playbackControl;
+    AsyncSceneLoader asyncSceneLoader;
+
+    void Awake()
+    {
+        // Set viewer to be persistent across scenes
+        // and delegate sceneLoaded to OnSceneLoad function (for tracking when scene changes)
+        DontDestroyOnLoad(transform.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoad;
+
+        timeline = GetComponent<PlayableDirector>();
+        playbackControl = GetComponent<PlaybackControl>();
+        asyncSceneLoader = GetComponent<AsyncSceneLoader>();
+    }
+
+    void OnSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        timeline.playableAsset = playables[sceneCounter];
+        timeline.time = 0f;
+        timeline.Play();
+
+        asyncSceneLoader.levelToLoad = "Scene_0" + (sceneCounter + 2).ToString();
+
+        sceneCounter++;
+    }
+}
